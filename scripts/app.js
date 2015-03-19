897dbe27649260d85a9025a744d4e02e7726063e
Matthias Langhard <matthias@langaust.com>
2015-03-19 11:41:54 +0100

    Initial Commit - Form from swubadsch

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var app = angular.module('app', ['ngRoute', 'ngStorage']);
+
+app.config(function ($routeProvider) {
+
+
+    $routeProvider.
+        when('/', {
+            templateUrl: '/views/chooseUser.html',
+            controller: 'ChooseUserCtrl'
+        }).
+        when('/opportunities', {
+            templateUrl: '/views/opportunities.html',
+            controller: 'OpportunitiesCtrl'
+        }).
+        when('/drinkevent/:swudrinkId', {
+            templateUrl: '/views/detail.html',
+            controller: 'DetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+});
+
+
+
